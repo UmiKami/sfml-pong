cb4fa06e9c5bf314ca3e0d20df7c365fb5cac54f
umikami <ggoku02@gmail.com>
2025-03-26 21:31:39 -0400

    Removed reference to test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
+#include <cmath>
+#include <iostream>
 #include <SFML/Graphics.hpp>
+
+
+void handleInput(float deltaTime);
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "PingPong with SFML| By UmiKami");
     window.setFramerateLimit(144);
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {
+        sf::Time elepased = clock.restart();
+        float deltaTime = elepased.asSeconds();
+
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
@@ -15,7 +25,53 @@ int main()
             }
         }
 
-        window.clear();
+        window.clear(sf::Color::Black);
+
+        handleInput( deltaTime);
+        // player.UpdateAnimation(deltaTime);
+        //
+        // player.Draw(window);
+
         window.display();
     }
+}
+
+bool IsKeyPressed(sf::Keyboard::Key key)
+{
+    return sf::Keyboard::isKeyPressed(key);
+}
+
+void handleInput(float deltaTime)
+{
+    sf::Vector2f inputDirection(0.f, 0.f);
+
+    if (IsKeyPressed(sf::Keyboard::Key::A))
+    {
+        inputDirection.x = -1;
+    }
+
+    if (IsKeyPressed(sf::Keyboard::Key::D))
+    {
+        inputDirection.x = 1;
+    }
+
+    if (IsKeyPressed(sf::Keyboard::Key::S))
+    {
+        inputDirection.y = 1;
+    }
+
+    if (IsKeyPressed(sf::Keyboard::Key::W))
+    {
+        inputDirection.y = -1;
+    }
+
+    // player->SetSpeed(100);
+    //
+    // if (inputDirection.x != 0 && inputDirection.y != 0)
+    // {
+    //     player->SetSpeed(player->GetSpeed() / std::sqrt(2.f));
+    // }
+    //
+    //
+    // player->Move(inputDirection, deltaTime);
 }
